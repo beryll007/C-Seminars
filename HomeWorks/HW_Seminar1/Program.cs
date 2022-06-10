caccc3191f0748688454889cef2bbbb637666bd8
Danil Novikov <beryl@live.ru>
2022-06-10 16:17:28 +0300

    Home work for first Seminar added

---
+++ HomeWorks/HW_Seminar1/Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача №1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/* 
+int num1, num2;
+
+Console.Write("Input first number: ");
+
+num1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Input second number: ");
+
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("The bigger number is " + num1);
+}
+else
+{
+    Console.WriteLine("The bigger number is " + num2);
+}
+// Если числа не целые? 
+*/
+
+// Задча №2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+int num1, num2, num3, max;
+
+Console.WriteLine("Input first number: ");
+
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input third number: ");
+
+num3 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+    if (num3 > max)
+    {
+        max = num3;
+    }
+}
+else max = num1;
+
+Console.WriteLine("Max number is: " + max);
+*/
+
+// Задача №3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+int a;
+
+Console.WriteLine("Input number you want to check if it is even : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("This number is even");
+}
+else
+{
+    Console.WriteLine("This number is odd");
+}
+*/
+
+// Задча №4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N, current;
+
+Console.WriteLine("Input any number and i will form in line all even numbers from 1 to yours: ");
+
+N = Convert.ToInt32(Console.ReadLine());
+
+
+while (N <= 1)
+{
+    Console.WriteLine("Nice try but input another one bigger than one! ");
+    N = Convert.ToInt32(Console.ReadLine()); ;
+}
+
+Console.WriteLine("Look! I lined up all even numbers from 1 to " + N + ":");
+current = 2;
+
+while (current <= N)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}
+
+
