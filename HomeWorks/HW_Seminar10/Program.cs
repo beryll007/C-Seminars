0316f1ecb3c71e50e4562308c6e427d43f44274c
Danil Novikov <beryl@live.ru>
2022-07-13 22:48:03 +0300

    Homework for Seminar 10 revised

--- HomeWorks/HW_Seminar10/Program.cs
+++ HomeWorks/HW_Seminar10/Program.cs
@@ -1,18 +1,18 @@
 ﻿
-string [] CreateStringArray(int size)
+string[] CreateStringArray(int size)
 {
     string[] words = new string[size];
 
     for (int i = 0; i < size; i++)
     {
-        Console.Write($"Input {i+1} words: ");
-        words [i] = Console.ReadLine();
+        Console.Write($"Input {i + 1} words: ");
+        words[i] = Console.ReadLine();
     }
 
     return words;
 }
 
-void ShowArray (string[] array)
+void ShowArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
@@ -23,14 +23,14 @@ void ShowArray (string[] array)
 //Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 //Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
-int AmountOfWordsStartingFromVowel (string[] array)
+int AmountOfWordsStartingFromVowel(string[] array)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         char Vowel = array[i].ToLower()[0];
-        if (Vowel == 'a' || Vowel == 'e' || Vowel == 'i' || Vowel == 'o' || Vowel == 'u' || Vowel == 'y') 
-        count ++;
+        if (Vowel == 'a' || Vowel == 'e' || Vowel == 'i' || Vowel == 'o' || Vowel == 'u' || Vowel == 'y')
+            count++;
     }
     return count;
 }
@@ -46,31 +46,40 @@ Console.WriteLine($"There are {AmountOfWordsStartingFromVowel(array)} words star
 //Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
 //Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
 
-string [] ArrayConverging (string[] array)
+string[] ArrayConverging(string[] array)
 {
-    int size = array.Length/2;
+    int size = array.Length / 2;
     //if (array.Length % 2 == 1) 
     //size = array.Length/2 + 1;
-    
     //else size = array.Length/2;
 
-    string [] NewArray = new string [size];
-    
+    string[] NewArray = new string[size];
+
     for (int i = 0; i < size; i++)
     {
-        NewArray [i] = array[i*2] + array [i*2+1];
-   
+        NewArray[i] = array[i * 2] + array[i * 2 + 1];
     }
 
     return NewArray;
 }
 
-string [] array = { "AB", "BA", "L", "P", "Limp", "Bizkit"};
-ShowArray(array);
+//string [] array = { "AB", "BA", "L", "P", "Limp", "Bizkit", "Nirvana"};
+//ShowArray(array);
 //Console.WriteLine(array.Length);
 
-string [] MyArray = ArrayConverging(array);
+//string [] MyArray = ArrayConverging(array);
+//ShowArray(MyArray);
+
+Console.WriteLine("This algorythm is merging two string array pairwise");
+Console.WriteLine("Input how many words you want your array to contain: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateStringArray(size);
+string[] MyArray = ArrayConverging(array);
+Console.WriteLine();
+Console.WriteLine($"Here is merged array: ");
 ShowArray(MyArray);
+if (array.Length % 2 == 1) Console.WriteLine($"The element '{array[array.Length - 1]}' doesn't have pair to merge with");
 
 
 
