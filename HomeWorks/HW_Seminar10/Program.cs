88d925311a22ec7d404898ceba40fc508628d821
Danil Novikov <beryl@live.ru>
2022-07-13 22:26:57 +0300

    Homework for Seminar 10 added

---
+++ HomeWorks/HW_Seminar10/Program.cs
@@ -0,0 +1,77 @@
+﻿
+string [] CreateStringArray(int size)
+{
+    string[] words = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i+1} words: ");
+        words [i] = Console.ReadLine();
+    }
+
+    return words;
+}
+
+void ShowArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+//Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+//Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
+
+int AmountOfWordsStartingFromVowel (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        char Vowel = array[i].ToLower()[0];
+        if (Vowel == 'a' || Vowel == 'e' || Vowel == 'i' || Vowel == 'o' || Vowel == 'u' || Vowel == 'y') 
+        count ++;
+    }
+    return count;
+}
+/*
+Console.WriteLine("This algorythm is to count words starting with the wovel letter");
+Console.WriteLine("Input how many words you want to check: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateStringArray(size);
+
+Console.WriteLine($"There are {AmountOfWordsStartingFromVowel(array)} words starting with the vowel.");
+*/
+//Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+//Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+string [] ArrayConverging (string[] array)
+{
+    int size = array.Length/2;
+    //if (array.Length % 2 == 1) 
+    //size = array.Length/2 + 1;
+    
+    //else size = array.Length/2;
+
+    string [] NewArray = new string [size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        NewArray [i] = array[i*2] + array [i*2+1];
+   
+    }
+
+    return NewArray;
+}
+
+string [] array = { "AB", "BA", "L", "P", "Limp", "Bizkit"};
+ShowArray(array);
+//Console.WriteLine(array.Length);
+
+string [] MyArray = ArrayConverging(array);
+ShowArray(MyArray);
+
+
+
+
